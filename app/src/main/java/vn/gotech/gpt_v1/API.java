99b5c42a583493bc18c08e2487982760372ff98c
anhhv <anhhv@gotech.vn>
2023-04-03 17:37:36 +0700

    Gpt v1

---
+++ app/src/main/java/vn/gotech/gpt_v1/API.java
@@ -0,0 +1,6 @@
+package vn.gotech.gpt_v1;
+
+public class API {
+    public static String API_URL = "https://api.openai.com/v1/completions";
+    public static String API = "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";
+}
